a30fed286aa5d52b766af4e3e4e5ab88d70003ae
MERT METİN <bjk.mert.metin@hotmail.com>
2021-07-05 01:44:10 +0300

    Add append method

--- MainTopics/stringMethods.py
+++ MainTopics/stringMethods.py
@@ -1,4 +1,7 @@
 isim=["aa","bb"] #String List
+isim.append("cc")
+print(isim)
 
-for i in range(len(isim)):
-    print((isim[i]).upper()) #Convert to uppercase of all character of the string value
+
+# for i in range(len(isim)):
+#     print((isim[i]).upper()) #Convert to uppercase of all character of the string value
