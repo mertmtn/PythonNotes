08056f883a5697dfecec8017b205950685540f53
MERT METİN <mertmetin1993@gmail.com>
2021-08-03 01:24:10 +0300

    Adding validating roman number function file

---
+++ GeneralAlgortithmExamples/ValidatingRomanNumber.py
@@ -0,0 +1,14 @@
+def IsValidRomanNumber(value):
+    thousand = "(?:(M){0,3})?"
+    hundred  = "(?:(D?(C){0,3})|(CM)|(CD))?"
+    ten      = "(?:(L?(X){0,3})|(XC)|(XL))?"
+    unit     = "(?:(V?(I){0,3})|(IX)|(IV))?"
+
+    regex_pattern = r"^" + thousand + hundred + ten + unit + "$"
+
+    import re
+
+    return bool(re.match(regex_pattern, value))
+
+
+print((IsValidRomanNumber("XII")))
